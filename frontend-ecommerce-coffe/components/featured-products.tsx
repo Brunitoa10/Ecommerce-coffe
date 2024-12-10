cad4038aad34d5627bd79ad4cf4954b265b54028
Bruno A Parisi <bnoprs00@gmail.com>
2024-12-10 04:59:24 -0300

    Intento fallido de traer datos de la API, efectos visuales frontend

--- frontend-ecommerce-coffe/components/featured-products.tsx
+++ frontend-ecommerce-coffe/components/featured-products.tsx
@@ -1,11 +1,35 @@
 "use client"
 
+import { useGetFeaturedProduct } from "@/api/useGetFeaturedProducts";
+
+import { ProductType } from "@/types/product";
+import { ResponseType } from "@/types/response";
+import SkeletonSchema from "./skeleton-Schema";
+import { Carousel, CarouselContent } from "./ui/carousel";
+
 const FeatureProducts = () => {
+  const { loading, result }: ResponseType = useGetFeaturedProduct();
+  
   return (
     <div className="max-w-6xl py-4 mx-auto sm:py-16 sm:px-24">
         <h3 className="px-6 text-3xl sm:pb-8">Productos destacados</h3>
+        <Carousel>
+          <CarouselContent className="-ml-2 md:-ml-4">
+            {loading && (
+              <SkeletonSchema grid={3}/>
+            )}
+            {result !== null && (
+              result.map((product: ProductType) => {
+                return(
+                  <p>Producto </p>
+                );
+              })
+            )}
+          
+          </CarouselContent>
+        </Carousel>
     </div>
-  )
+  );
 }
 
 export default FeatureProducts;
