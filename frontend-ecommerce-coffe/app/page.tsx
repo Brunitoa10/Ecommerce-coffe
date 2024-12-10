04ccdf0815bc31e960d534a891d331bc85edf7f2
Bruno A Parisi <bnoprs00@gmail.com>
2024-12-09 22:55:50 -0300

    Cambios backend y frontend

--- frontend-ecommerce-coffe/app/page.tsx
+++ frontend-ecommerce-coffe/app/page.tsx
@@ -1,9 +1,11 @@
 import CarrouselTextBanner from "@/components/carrousel-text-banner";
+import FeatureProducts from "@/components/featured-products";
 
 export default function Home() {
   return (
     <main>
      <CarrouselTextBanner/>
+     <FeatureProducts/>
     </main>
   );
 }
---
+++ frontend-ecommerce-coffe/components/featured-products.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+const FeatureProducts = () => {
+  return (
+    <div className="max-w-6xl py-4 mx-auto sm:py-16 sm:px-24">
+        <h3 className="px-6 text-3xl sm:pb-8">Productos destacados</h3>
+    </div>
+  )
+}
+
+export default FeatureProducts;
